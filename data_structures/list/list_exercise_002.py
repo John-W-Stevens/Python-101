a833f86f7510f28fec07a3bccddc4199aff5828e
John Stevens <john@Johns-MacBook-Pro.local>
2023-09-08 16:19:28 -0700

    fixed typo

--- data_structures/list/list_exercise_002.py
+++ data_structures/list/list_exercise_002.py
@@ -4,4 +4,4 @@ names = ["Bob", "Susan", "Charlie", "Edward", "Chuck", "Andrew"]
 
 # 1. What value is at the first index position in 'names' object?
 
-# 2. Is is more efficient to retrieve entries from this list using index or value lookup? Why?
+# 2. Is it more efficient to retrieve entries from this list using index or value lookup? Why?
