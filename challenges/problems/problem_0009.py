277b6315c5e271cbeef835d97cbfd35eb384ec2a
John Stevens <john@Johns-MacBook-Pro.local>
2023-09-08 16:42:17 -0700

    added two_sum problem

---
+++ challenges/problems/problem_0009.py
@@ -0,0 +1,8 @@
+# Complete the function two_sum that takes a list of intergers and a target integer
+# and returns a list of the index positions of the two numbers in the list that add up to the target.
+
+def two_sum(list_of_numbers, target_number):
+
+    # your code here
+
+    return []
